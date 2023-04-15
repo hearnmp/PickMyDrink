664b47409d206e1f764218fad36d3c90d8345985
hearnmp <117321628+hearnmp@users.noreply.github.com>
2023-04-15 15:58:49 -0400

    style

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="flex w-full items-center justify-center h-8">
+    <div className="flex w-full items-center justify-center h-8 mt-40">
       <h1 className="text-xl">This app uses the Cocktail API v1 from <a className="underline" href="https://api-ninjas.com/">API-Ninjas</a></h1>
     </div>
   );
